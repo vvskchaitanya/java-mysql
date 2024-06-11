3f8a5e96638be742326e3244deab4d058dfd4bd6
vvskchaitanya <vvskchaitanya@gmail.com>
2024-06-11 16:49:08 +0000

    commands

--- JavaMysqlDatabase/src/com/fullstack/sql/JavaMysqlExample.java
+++ JavaMysqlDatabase/src/com/fullstack/sql/JavaMysqlExample.java
@@ -3,12 +3,13 @@ package com.fullstack.sql;
 public class JavaMysqlExample {
 
     public static void main(String[] args) {
-        create("Laptop", 1200.50, 10);
-        read();
-        update(1, "Gaming Laptop", 1500.75, 5);
-        read();
-        delete(1);
-        read();
+
+        // Add Product into Products Table
+        ProductsService.create("Laptop", 1200.50, 10);
+
+        // Read from the Products table
+        ProductsService.read();
+
     }
 
 }
--- mysql-commands.sql
+++ mysql-commands.sql
@@ -1,3 +1,14 @@
 CREATE USER 'user1'@'localhost' IDENTIFIED BY 'password';
 GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT ON *.* TO 'user1'@'localhost' WITH GRANT OPTION;
+
+
 CREATE DATABASE fullstack;
+
+USE javaproject;
+
+CREATE TABLE products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL
+);
